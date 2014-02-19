0b0b28a76f9f042297ee8a10c9b6b4bcf011dc71
internity <mohamedaminearbi@gmail.com>
2014-02-19 20:17:20 +0100

    Merge origin/master
    Conflicts:
    	pi_dev_test/src/pi_dev_test/Pi_dev_test.java

--- src/pi/bestdeal/dao/DealDAO.java
+++ src/pi/bestdeal/dao/DealDAO.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pi.bestdeal.dao;
 
 import java.sql.ResultSet;
@@ -21,15 +20,15 @@ import pi.bestdeal.utils.MySQLConnection;
  * @author Internet
  */
 public class DealDAO {
-    
-    public List<Deal> displayDeal(){
+
+    public List<Deal> displayDeal() {
         List<Deal> dealListe = new ArrayList<Deal>();
         String sqlrequest = "SELECT * FROM pi_dev.deal";
         Statement statement;
         try {
             statement = MySQLConnection.getInstance().createStatement();
             ResultSet resultat = statement.executeQuery(sqlrequest);
-            while (resultat.next()){
+            while (resultat.next()) {
                 Deal deal = new Deal();
                 deal.setIdDeal_Deal(resultat.getInt("idDeal"));
                 deal.setTitreDeal_Deal(resultat.getString("titreDeal"));
@@ -41,27 +40,18 @@ public class DealDAO {
                 deal.setStatutDeal_Deal(resultat.getBoolean("StatutDeal"));
                 deal.setDateDebutDeal_Deal(resultat.getDate("dateDebut"));
                 deal.setDateFinDeal_Deal(resultat.getDate("dateFin"));
-                deal.setNbrAffichage_Deal(Integer.valueOf(resultat.getString("nbrAffichage")));
+                deal.setNbrAffichage_Deal(resultat.getInt("nbrAffichage"));
+                deal.setIdVendeur_Deal(resultat.getInt("idVendeur"));
                 
+                dealListe.add(deal);
                 
-                    /*
-              
-  `nbrAchatValidation` int(11) NOT NULL,
-  `etatDeal` varchar(45) NOT NULL,
-  `StatutDeal` tinyint(1) NOT NULL,
-  `dateDebut` datetime NOT NULL,
-  `dateFin` datetime NOT NULL,
-  `nbrAffichage` varchar(45) DEFAULT NULL,
-  `idVendeur` int(11) DEFAULT '0',
-  PRIMARY KEY (`idDeal`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-                */
             }
+            
         } catch (SQLException ex) {
             Logger.getLogger(DealDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        return dealListe;
+
     }
-    
+
 }
