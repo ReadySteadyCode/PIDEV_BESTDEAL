70468debc0af055036474dd3ce2d39ffa62fc9f3
internity <mohamedaminearbi@gmail.com>
2014-02-21 13:40:26 +0100

    Merge origin/master
    Conflicts:
    	pi_dev_test/src/pi_dev_test/Pi_dev_test.java

--- src/pi/bestdeal/dao/ClientDAO.java
+++ src/pi/bestdeal/dao/ClientDAO.java
@@ -6,10 +6,33 @@
 
 package pi.bestdeal.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pi.bestdeal.utils.MySQLConnection;
+
 /**
  *
  * @author Internet
  */
-public class ClientDAO {
-    //jhsdbvjhdc
+public class ClientDAO
+{
+
+    public ClientDAO() {
+    }
+        
+    public int deleteClient (int idClient)
+    {
+        int a=0;
+      String sqlrequest = "DELETE FROM pi_dev.client WHERE idClient=?;";
+        try {
+            PreparedStatement ps = MySQLConnection.getInstance().prepareStatement(sqlrequest);
+            ps.setInt(1, idClient);
+           a = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ClientDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return a;
+    }
 }
