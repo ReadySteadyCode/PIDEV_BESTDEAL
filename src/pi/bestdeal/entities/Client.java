f9f409bd21d3dfed9c457b7a919c541527378fab
internity <mohamedaminearbi@gmail.com>
2014-02-24 21:58:10 +0100

    Merge origin/master
    Conflicts:
    	pi_dev_test/src/pi_dev_test/Pi_dev_test.java

--- src/pi/bestdeal/entities/Client.java
+++ src/pi/bestdeal/entities/Client.java
@@ -7,6 +7,7 @@
 package pi.bestdeal.entities;
 
 import java.util.Date;
+import java.util.Objects;
 import javax.xml.crypto.Data;
 
 /**
@@ -17,6 +18,11 @@ public class Client {
     
   
    private int idClient; 
+
+    public Client() {
+    }
+   
+   
 
     public void setIdClient(int idClient) {
         this.idClient = idClient;
@@ -58,10 +64,7 @@ public class Client {
         return idClient;
     }
 
-    @Override
-    public String toString() {
-        return "Client{" + "idClient=" + idClient + ", nom=" + nom + ", prenom=" + prenom + ", password=" + password + ", Telephone=" + Telephone + ", email=" + email + ", listedechoix=" + listedechoix + ", CIN=" + CIN + ", datenaissance=" + datenaissance + '}';
-    }
+    
 
     public String getNom() {
         return nom;
@@ -102,6 +105,65 @@ public class Client {
   private  String listedechoix ;
   private int CIN ;
   private  Date datenaissance ;
+  private Boolean sexe;
+
+    public Boolean getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(Boolean sexe) {
+        this.sexe = sexe;
+    }
+
+    public Client(int idClient, String nom, String prenom, String password, String Telephone, String email, String listedechoix, int CIN, Date datenaissance, Boolean sexe) {
+        this.idClient = idClient;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.password = password;
+        this.Telephone = Telephone;
+        this.email = email;
+        this.listedechoix = listedechoix;
+        this.CIN = CIN;
+        this.datenaissance = datenaissance;
+        this.sexe = sexe;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "idClient=" + idClient + ", nom=" + nom + ", prenom=" + prenom + ", password=" + password + ", Telephone=" + Telephone + ", email=" + email + ", listedechoix=" + listedechoix + ", CIN=" + CIN + ", datenaissance=" + datenaissance + ", sexe=" + sexe + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Client other = (Client) obj;
+        if (this.idClient != other.idClient) {
+            return false;
+        }
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.prenom, other.prenom)) {
+            return false;
+        }
+        if (this.CIN != other.CIN) {
+            return false;
+        }
+        return true;
+    }
+  
+ 
   
  
 
