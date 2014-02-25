cabb85fdbbb56276abea64641f99221fc74ee71a
internity <mohamedaminearbi@gmail.com>
2014-02-25 13:52:51 +0100

    Merge origin/master
    Conflicts:
    	pi_dev_test/src/pi_dev_test/Pi_dev_test.java

--- src/pi/bestdeal/dao/VendeurDAO.java
+++ src/pi/bestdeal/dao/VendeurDAO.java
@@ -6,7 +6,10 @@
 package pi.bestdeal.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import pi.bestdeal.entities.Vendeur;
@@ -50,6 +53,7 @@ public class VendeurDAO {
      @param idVendeur
      * @return int 0: un problème survient, 1: ajout avec succès
      */
+    
     public int deleteVendeur(int idVendeur)
     {
          int a=0;
@@ -62,6 +66,58 @@ public class VendeurDAO {
             Logger.getLogger(ClientDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return a;
+    }
+    /**
+     * 
+     * @param vendeur
+     * @return int 0: un problème survient, 1: ajout avec succès
+     */
+    public int updateVendeur (Vendeur vendeur)
+    {
+        int a=0;
+        String sqlrequest = "UPDATE `pi_dev`.`vendeur` SET `nomCommercial`=?, `addresse`=?, `typeBien`=?, `description`=?, `note`=? WHERE `idVendeur`=?;";
+        try {
+            PreparedStatement ps = MySQLConnection.getInstance().prepareStatement(sqlrequest);
+            ps.setString(1, vendeur.getNomCommercial());
+            ps.setString(2, vendeur.getAddresse());
+            ps.setString(3, vendeur.getTypeBien());
+            ps.setString(4, vendeur.getDescription());
+            ps.setInt(5, vendeur.getIdVendeur());
+            
+            a= ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(VendeurDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+        return a;
+    }
+    
+    /**
+     * 
+     * @return List<Vendeur>
+     */
+    public List<Vendeur> displayVendeur ()
+    {
+        List<Vendeur> listevendeur = new ArrayList<>();
+        String sqlrequest ="SELECT * FROM pi_dev.vendeur;";
+        try {
+            PreparedStatement ps = MySQLConnection.getInstance().prepareStatement(sqlrequest);
+            ResultSet resualt = ps.executeQuery();
+            while(resualt.next())
+            {
+            Vendeur vendeur = new Vendeur();
+            vendeur.setIdVendeur(resualt.getInt("idVendeur"));
+            vendeur.setAddresse(resualt.getString("addresse"));
+            vendeur.setDescription(resualt.getString("description"));
+            vendeur.setNomCommercial(resualt.getString("nomCommercial"));
+            vendeur.setNote(resualt.getInt("note"));
+           vendeur.setTypeBien(resualt.getString("typeBien"));
+           listevendeur.add(vendeur);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(VendeurDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listevendeur;
     }
 
 }
