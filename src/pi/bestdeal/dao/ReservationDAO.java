07b8c1274e6c7ba11981f8e656196fa106fbda33
internity <mohamedaminearbi@gmail.com>
2014-03-05 22:08:38 +0100

    Merge origin/master
    Amine : MESSAGEDAO: correction de la requete d'affichage des messages par nom

--- src/pi/bestdeal/dao/ReservationDAO.java
+++ src/pi/bestdeal/dao/ReservationDAO.java
@@ -6,6 +6,13 @@
 
 package pi.bestdeal.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pi.bestdeal.utils.MySQLConnection;
+
 /**
  *
  * @author Internet
@@ -18,6 +25,26 @@ public class ReservationDAO {
             reservationDAO = new ReservationDAO();
         }
         return reservationDAO;
+    }
+    public int reservationCounterByGender (Boolean gender,int idDeal){
+        int rs=0;
+    String sqlrequest="SELECT count(pi_dev.reservation.idReservation) as compteur FROM pi_dev.reservation,pi_dev.Deal where pi_dev.reservation.IdDeal=pi_dev.Deal.idDeal and pi_dev.Deal.idDeal=? and pi_dev.reservation.dateRerservation between ? and ?;";
+ try {
+            PreparedStatement ps = MySQLConnection.getInstance().prepareStatement(sqlrequest);
+            ps.setBoolean(1, gender);
+            ps.setInt(2, idDeal);
+            ResultSet result = ps.executeQuery();            
+           
+            while (result.next())
+            {
+                rs=result.getInt("compteur");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ConsultationDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+           return rs; 
     }
     
 }
